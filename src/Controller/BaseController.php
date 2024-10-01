a9d83c4e4c21cfc9f92a8a6e0ff0de7493e234f9
Gwendal <gwendalferlin20@gmail.com>
2024-10-01 11:57:17 +0200

    commit final

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -1,9 +1,13 @@
 <?php
 namespace App\Controller;
 
+use App\Form\AjoutBonbonType;
+use App\Repository\TypeBonbonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\TypeBonbon;
+
 
 class BaseController extends AbstractController
 {
@@ -15,4 +19,23 @@ class BaseController extends AbstractController
             'controller_name' => 'BaseController', // Le contrôleur donne à la vue une variable dont le contenu est BaseController, cela nous ne servira pas, nous l’enlèverons un peu plus loin
         ]);
     }
+
+    #[Route('/ajoutBonbon', name: 'app_ajout_bonbon')]
+    public function contact(): Response
+    {
+        $form = $this->createForm(AjoutBonbonType::class);
+        return $this->render('base/ajout_bonbon.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    #[Route('/liste-typebonbon', name: 'app_liste-type_bonbon')]
+    public function listeTypeBonbon(TypeBonbonRepository $contactRepository): Response
+    {
+        $bonbons = $contactRepository->findAll();
+        return $this->render('base/liste-type_bonbon.html.twig', [
+            'bonbons' => $bonbons
+
+        ]);
+    }
+
 }
